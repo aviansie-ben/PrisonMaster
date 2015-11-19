bae587419ded69463dab499071f4dc5b22cd9df2
bendude56 <bendude56@gmail.com>
2015-11-19 23:41:53 +0000

    Replace tabs with spaces

--- pmaster/models.py
+++ pmaster/models.py
@@ -11,65 +11,65 @@ class Prison(db.Model):
         return '<Prison ' + id + '>'
 
 class Prisoner(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	first_name = db.Column(db.Unicode(64))
-	last_name = db.Column(db.Unicode(64))
-	release_date = db.Column(db.Date())
-	
-	def __repr__(self):
-		return '<Prisoner ' + id + '>'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Unicode(64))
+    last_name = db.Column(db.Unicode(64))
+    release_date = db.Column(db.Date())
+    
+    def __repr__(self):
+        return '<Prisoner ' + id + '>'
 
 class Cell(db.Model):
-	capacity = db.Column(db.Integer)
-	number = db.Column(db.Integer, primary_key=True)
-	
-	def __repr__(self):
-		return '<Cell ' + number + '>'
-		
+    capacity = db.Column(db.Integer)
+    number = db.Column(db.Integer, primary_key=True)
+    
+    def __repr__(self):
+        return '<Cell ' + number + '>'
+        
 class UserAccount(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.Unicode(64), index=True, unique=True)
-	is_active = db.Column(db.Boolean())
-	password_hash = db.Column(db.Text())
-	
-	def __repr__(self):
-		return '<User Account ' + id + '>'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Unicode(64), index=True, unique=True)
+    is_active = db.Column(db.Boolean())
+    password_hash = db.Column(db.Text())
+    
+    def __repr__(self):
+        return '<User Account ' + id + '>'
 
 class Employee(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	security_clearance = db.Column(db.Integer)
-	position = db.Column(db.UnicodeText)
-	first_name = db.Column(db.Unicode(64))
-	last_name = db.Column(db.Unicode(64))
-	
-	def __repr__(self):
-		return '<Employee ' + id + '>'
+    id = db.Column(db.Integer, primary_key=True)
+    security_clearance = db.Column(db.Integer)
+    position = db.Column(db.UnicodeText)
+    first_name = db.Column(db.Unicode(64))
+    last_name = db.Column(db.Unicode(64))
+    
+    def __repr__(self):
+        return '<Employee ' + id + '>'
 
 class AccessCard(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	security_clearance = db.Column(db.Integer)
-	expiry_date = db.Column(db.Date)
-	
-	def __repr__(self):
-		return '<Access Card ' + id + '>'
-		
+    id = db.Column(db.Integer, primary_key=True)
+    security_clearance = db.Column(db.Integer)
+    expiry_date = db.Column(db.Date)
+    
+    def __repr__(self):
+        return '<Access Card ' + id + '>'
+        
 class AccessLog(db.Model):
-	timestamp = db.Column(db.DateTime(), primary_key=True)
-	
-	def __repr__(self):
-		return '<Access Log ' + timestamp + '>'
-		
+    timestamp = db.Column(db.DateTime(), primary_key=True)
+    
+    def __repr__(self):
+        return '<Access Log ' + timestamp + '>'
+        
 class AccessPoint(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	security_clearance = db.Column(db.Integer)
-	
-	def __repr__(self):
-		return '<Access Point ' + id + '>'
-		
+    id = db.Column(db.Integer, primary_key=True)
+    security_clearance = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return '<Access Point ' + id + '>'
+        
 class Schedule(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	time_open = db.Column(db.DateTime())
-	time_close = db.Column(db.DateTime())
-	
-	def __repr__(self):
-		return '<Schedule ' + id + '>'
+    id = db.Column(db.Integer, primary_key=True)
+    time_open = db.Column(db.DateTime())
+    time_close = db.Column(db.DateTime())
+    
+    def __repr__(self):
+        return '<Schedule ' + id + '>'
